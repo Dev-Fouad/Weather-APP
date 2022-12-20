e6f675d6b0f84c2ebd36e50f0cf021f34271e04b
fouad ajayi <fouadajayi6@gmail.com>
2022-12-20 13:19:04 +0100

    Made new changes

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ let getWeather = async (id) => {
 
     let base = 'http://dataservice.accuweather.com/currentconditions/v1/{locationKey}'
     let query = `${id}?apikey=${key}`
+
+    let response = await fetch(base + query)
+    let data = await response.json()
+    return data
 } 
 
 // Get city information
@@ -19,6 +23,54 @@ let getcity = async (city) => {
     return data[0]
 }
 
-getcity('lagos')
-    .then((data) => console.log(data))
-    .catch((err) => console.log(er))
+
+
+
+let cityform = document.querySelector('form')
+let card = document.querySelector('.card')
+let details = document.querySelector('.details')
+let time = document.querySelector('times')
+let updateUI = async (data) => {
+    let {citydets , weather} = data
+
+    // Update details template
+    details.innerHTML = `
+        <h5 class="my-3">${citydets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `
+
+    // Remove the d-none class if present
+    if (card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+}
+
+let updatecity = async (city) => {
+
+    let citydets = await getcity(city)
+    let weather = await getWeather(citydets.key) 
+
+    return{
+        citydets: citydets,
+        weather: weather
+    }
+}
+
+
+cityform.addEventListener('submit' , e => {
+    // Prevent from default
+    e.preventDefault()
+
+    // get city value
+    let city = cityform.city.value.trim()
+    cityform.reset()
+
+    // update UI WITH NEW CITY
+    updatecity(city)
+      .then(data => updateUI(data))
+      .catch(err => console.log(err))
+})
